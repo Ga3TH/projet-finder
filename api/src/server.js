bdf416839256812aabd12ff63727923140ffd373
gaeth-hub <gaeth0311@gmail.com>
2026-09-16 21:54:38 +0200

    Etape 4 fini

--- api/src/server.js
+++ api/src/server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 const prisma = new PrismaClient();
 
-// Middleware pour lire le JSON
 app.use(express.json());
 
 app.get("/health", (req, res) => {
